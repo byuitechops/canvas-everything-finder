cf48334e81b73911a45afdd4eaaa60133ebde72e
Aaron Shiffler <ajshiffler@gmail.com>
2018-10-29 15:15:06 -0600

    Development First Push

--- deepSearch.js
+++ deepSearch.js
@@ -6,5 +6,21 @@ const asynclib = require('async');
 const canvas = require('canvas-api-wrapper');
 
 module.exports = function deepSearch(searchPhrase, canvasItems) {
-    canvasItems;
+    function recursiveSearch(canvasItem) {
+        // Define what to do if the item is an object
+        function recursionOnObject (canvasItem) {
+            Object.keys(canvasItem);
+        }
+
+        // 
+        function recursionOnValue  (canvasItem) {}
+
+        //
+        if ( Array.isArray(canvasItem) ) return recursionOnArrary(canvasItem);
+        else if ( canvasItem !== null && typeof canvasItem === 'object' ) return recursionOnObject(canvasItem);
+        else return recursionOnValue(canvasItem);
+    }
+    return canvasItems.reduce( (acc, canvasItem) => {
+        return acc.concat( recursiveSearch(canvasItem) );
+    }, [] );
 };
