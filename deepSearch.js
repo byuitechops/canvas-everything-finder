b37cb9f48f0bffada896d97272cbdd9f26c4314a
Aaron Shiffler <ajshiffler@gmail.com>
2018-11-02 16:26:21 -0600

    Solved inconsistencies in deepsearch function to make it more intuitive to use and debug with

--- deepSearch.js
+++ deepSearch.js
@@ -1,30 +1,36 @@
 /*************************************************************************
- * Gets all the course's item[key] details, flatten them, then search through
- * each item[key] searching for a match
+ * Searches through an object using recursion any time an object is found 
  *************************************************************************/
 
-module.exports = function deepSearch(searchPhrase, searchItem) {
-    var item = [].concat(searchItem);
-    
-    function compareValues (searchPhrase, searchValue) {
-        if (typeof searchValue === 'undefined') searchValue = typeof undefined;
+module.exports = function deepSearch(searchItem, searchPhrase) {
+    /*********************************************************************
+     * Stringifies the item passed in, and compares it through RegEx
+     *********************************************************************/
+    function compareValues (searchValue, searchPhrase) {
+        const specialValues = {null: 'null', undefined: 'undefined'}; // Non-Object Values that don't have a toString method
+        if (specialValues[searchValue ]) searchValue  = specialValues[searchValue ];
+        if (specialValues[searchPhrase]) searchPhrase = specialValues[searchPhrase];
+        // Preprare Regex
         let search = typeof searchPhrase === 'string' ? searchPhrase : searchPhrase.toString();
-        let value = typeof searchValue === 'string' ? searchValue : searchValue.toString();
+        let value  = typeof searchValue  === 'string' ? searchValue  : searchValue.toString() ;
         let searchExp = new RegExp (search, 'i');
+        // Test Phrases
         return searchExp.test( value );
     }
 
+    /*********************************************************************
+     * 
+     *********************************************************************/
     function recursiveSearch (item, accumulator, searchPath = []) {
         if (typeof item === 'object' && item !== null) {
             Object.keys(item).forEach( key => { recursiveSearch(item[key], accumulator, searchPath.concat(key)); } );
-        } else if (compareValues(searchPhrase, item)) {
+        } else if (compareValues(item, searchPhrase)) {
             accumulator.push({match: item, path: searchPath});
         }
     }
-    
-    return Object.keys(item).reduce( (acc, key) => {
-        recursiveSearch(item[key], acc, [].concat(key));
-        return acc;
-    }, [] );
+
+    var searchMatches = []; // The thing to hold data between recursions
+    recursiveSearch(searchItem, searchMatches, []);
+    return searchMatches;
 
 };
