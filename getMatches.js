0d407b0d1f968315b057a67d4ae882233582a306
ajshiff <ajshiffler@gmail.com>
2019-03-18 17:55:44 -0600

    Added more searches

--- getMatches.js
+++ getMatches.js
@@ -4,45 +4,60 @@
  *************************************************************************/
 const path = require('path');
 const canvas = require('canvas-api-wrapper');
-const deepSearch = require( path.join(__dirname, '/deepSearch.js') );
-const limitObjectKeys = require( path.join(__dirname, '/limitObjectKeys.js') );
+const deepSearch = require(path.join(__dirname, '/deepSearch.js'));
+const limitObjectKeys = require(path.join(__dirname, '/limitObjectKeys.js'));
 
-function makeOutputObject (courseObject, itemObject, matchData, searchPhrase, apiCall, message = '') {
+function makeOutputObject(courseObject, itemObject, matchData, searchPhrase, apiCall, message = '') {
     return {
-        course       : courseObject, 
-        item         : itemObject, 
-        matchData    : matchData, 
-        searchPhrase : searchPhrase, 
-        apiCall      : apiCall, 
-        message      : message,
+        course: courseObject,
+        item: itemObject,
+        matchData: matchData,
+        searchPhrase: searchPhrase,
+        apiCall: apiCall,
+        message: message,
     };
 }
 
+async function getSubItems(initialId, subItemKey, initialApiCall, secondaryApiCall) {
+    let courseItems = await canvas.get(initialApiCall(initialId));
+    let apiCalls = courseItems.map((item) => secondaryApiCall(initialId, item[subItemKey]));
+    return apiCalls;
+}
+
+
 module.exports = async function getCourseItems(course, searchPhrase) {
     // Define API Calls Here. Listed as an object to have readable named values
-    var canvasApiCalls = {
-        getAssignments: `/api/v1/courses/${course.id}/assignments`,
-        listPages: `/api/v1/courses/${course.id}/pages`,
-        listModules: `/api/v1/courses/${course.id}/modules?include[]=items`,
-        // listQuizzes: `/api/v1/courses/${course.id}/quizzes`
-    };
+    var canvasApiCalls = [
+        `/api/v1/courses/${course.id}/assignments`, // getAssignments
+        // `/api/v1/courses/${course.id}/pages`, // listPages
+        `/api/v1/courses/${course.id}/modules`, // listModules
+        `/api/v1/courses/${course.id}/quizzes`, // getQuizzes
+        `/api/v1/courses/${course_id}/discussion_topics`, // getDiscussionTopics (aka discussion boards)
+        ...await getSubItems(course_id, 'url', (initialId) => `/api/v1/courses/${initialId}/pages`, (initialId, subId) => `/api/v1/courses/${initialId}/quizzes/${subId}/questions`),
+        ...await getSubItems(course.id, 'id', (initialId) => `/api/v1/courses/${initialId}/quizzes`, (initialId, subId) => `/api/v1/courses/${initialId}/quizzes/${subId}/questions`),
+        ...await getSubItems(course.id, 'id', (initialId) => `/api/v1/courses/${initialId}/modules/`, (initialId, subId) => `/api/v1/courses/${initialId}/modules/${subId}/items`),
+    ];
+    // let getQuizQuestions = await getSubItems(course.id, (initialId) => `/api/v1/courses/${initialId}/quizzes`, (initialId, subId) => `/api/v1/courses/${initialId}/quizzes/${subId}/questions`); // getQuizQuestions
+    // let getModuleItems = await getSubItems(course.id, (initialId) => `/api/v1/courses/${initialId}/modules/`, (initialId, subId) => `/api/v1/courses/${initialId}/modules/${subId}/items`); // getModuleItems
+    // canvasApiCalls = canvasApiCalls.concat(getQuizQuestions, getModuleItems)
+    // console.log(canvasApiCalls)
 
     // Core: Search, scan, report
     var allMatches = [];
-    var outputKeys = ['id','name','html_url',];
+    var outputKeys = ['id', 'name', 'html_url',];
     for (let apiCall in canvasApiCalls) { // for in opted for to avoid having to do: promise.all(array.method(async () => {} ))
         let canvasData = await canvas.get(canvasApiCalls[apiCall]);
-        allMatches = canvasData.reduce( (acc, data) => {
+        allMatches = canvasData.reduce((acc, data) => {
             let matches = deepSearch(data, searchPhrase);
             let outputData = limitObjectKeys(data, outputKeys);
-            matches.forEach( (match) => acc = acc.concat( makeOutputObject(course, outputData, match, searchPhrase, canvasApiCalls[apiCall]) ) );
+            matches.forEach((match) => acc = acc.concat(makeOutputObject(course, outputData, match, searchPhrase, canvasApiCalls[apiCall])));
             return acc;
         }, []);
     }
 
     // If no matches were found in this course, tag the outputTemplate
     // onto the output so that the course is represented on the output
-    if (allMatches.length === 0) allMatches.push( makeOutputObject(course, {}, {}, searchPhrase, '') );
+    if (allMatches.length === 0) allMatches.push(makeOutputObject(course, {}, {}, searchPhrase, ''));
 
     return allMatches;
 };
--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const path = require('path');
 const d3 = require('d3-dsv');
 // const asynclib = require('async');
 // const getMatchesAsyncLib = require( path.join(__dirname, '/getMatchesAsyncLib.js') );
-const cli                = require( './cli.js'               );
-const getCourses         = require( './getCourses.js'        );
-const getMatches         = require( './getMatches.js'        );
-const getMemoryStats     = require( './getMemoryStats.js'    );
-const promiseQueueLimit  = require( './promiseQueueLimit.js' );
-const repeatOnInterval   = require( './repeatOnInterval.js'  );
+const cli = require('./cli.js');
+const getCourses = require('./getCourses.js');
+const getMatches = require('./getMatches.js');
+const getMemoryStats = require('./getMemoryStats.js');
+const promiseQueueLimit = require('./promiseQueueLimit.js');
+const repeatOnInterval = require('./repeatOnInterval.js');
 
 
 
 // TODO Abstract cli input and csv output parts of this function and let them be run in a true index file so that this can be more modular
 async function main() {
-    var queueLimit = 5000;
+    var queueLimit = 10;
     var courseCounter = 0;
     var userInput = await cli();
     // repeatOnInterval([getMemoryStats], 5000); // Start Tracking Memory Usage
@@ -23,23 +23,23 @@ async function main() {
 
     console.log(`\nYou have found ${courseList.length} courses!\n`);
 
-    async function getMatchesAdapter (course) {
-        return new Promise( async (resolve, reject) => {
+    async function getMatchesAdapter(course) {
+        return new Promise(async (resolve, reject) => {
             getMatches(course, userInput.searchPhrase)
-                .then ( (matchData) => {
+                .then((matchData) => {
                     console.log(`${++courseCounter}/${courseList.length}, ${course['name']} Completed`);
                     resolve(matchData);
-                } )
-                .catch( (error) => {
+                })
+                .catch((error) => {
                     console.log(`${++courseCounter}/${courseList.length}, ${course['name']} Completed`);
-                    reject( {course: course, item: {}, matchData: {}, searchPhrase: userInput.searchPhase, apiCall: '', message: error} );
-                } );
-        } ); 
+                    reject({ course: course, item: {}, matchData: {}, searchPhrase: userInput.searchPhase, apiCall: '', message: error });
+                });
+        });
     }
 
     await promiseQueueLimit(courseList, getMatchesAdapter, queueLimit, closingSteps);
 
-    function closingSteps (err, matches) {
+    function closingSteps(err, matches) {
         matches = matches.map(item => {
             return {
                 'course.id': item.course.id,
